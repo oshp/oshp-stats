1514c6cd23d29250a71e14af17433f53abf337c4
drighetto <drighetto@excellium-services.com>
2022-03-20 10:27:13 +0100

    Add infos

--- scripts/generate_stats_md_file.py
+++ scripts/generate_stats_md_file.py
@@ -162,9 +162,9 @@ def compute_hsts_average_maxage_global_usage():
 if __name__ == "__main__":
     init_stats_file()
     OSHP_SECURITY_HEADERS.sort()
+    compute_secure_headers_global_usage()
     for header_name in OSHP_SECURITY_HEADERS:
         compute_header_global_usage(header_name)
     compute_insecure_framing_configuration_global_usage()
     compute_hsts_preload_global_usage()
-    compute_secure_headers_global_usage()
     compute_hsts_average_maxage_global_usage()
